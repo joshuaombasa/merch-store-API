d5aa2c7920e8f9bfd1acd2c9eeea00252a62c95e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-07 14:51:28 +0300

    added main server file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const port = 3000
+const cors = require('cors')
+
+app.use(express.json())
+
+const app = express()
+
+const ordersRoutes = require('./routes/orders')
+const loginRoutes = require('./routes/login')
+const order_itemsRoutes = require('./routes/order_items')
+const signupRoutes = require('./routes/signup')
+const usersRoutes = require('./routes/users')
+const productsRoutes = require('./routes/products')
+const categoriesRoutes = require("./routes/catogories")
+
+app.use('/api/login', loginRoutes)
+app.use('/api/orders', ordersRoutes)
+app.use('/api/order_items', order_itemsRoutes)
+app.use('/api/signup', signupRoutes)
+app.use('/api/users', usersRoutes)
+app.use('/api/products', productsRoutes)
+app.use('/api/catogories', categoriesRoutes)
+
+app.listen(port, () => {
+    console.log(`Server listening on port 3000`)
+})
